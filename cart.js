c086cd0b2f68aeb76952e3e58d23783de92699f7
Harmitha <harmithavenkatesan5@gmail.com>
2026-05-07 14:08:29 +0530

    connected frontend

---
+++ cart.js
@@ -0,0 +1,41 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const container = document.getElementById("cart-items");
+
+let total = 0;
+
+cart.forEach((product, index) => {
+  total += product.price;
+
+  container.innerHTML += `
+    <div class="card">
+      <img src="${product.image}" width="150">
+      <h3>${product.name}</h3>
+      <p>₹${product.price}</p>
+
+      <button onclick="increase(${index})">+</button>
+      <button onclick="decrease(${index})">-</button>
+    </div>
+  `;
+});
+
+document.getElementById("total").innerText =
+  "Total: ₹" + total;
+
+// INCREASE
+function increase(index) {
+  cart.push(cart[index]);
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  location.reload();
+}
+
+// DECREASE
+function decrease(index) {
+  cart.splice(index, 1);
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  location.reload();
+}
